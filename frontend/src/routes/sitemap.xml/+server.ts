39a238bee9f1e4d0d2abbf611070d34a67e59773
Jakub Wegrzyn <jakubek.weg@gmail.com>
2023-02-12 10:52:56 +0100

    Improve sitemap for google

--- frontend/src/routes/sitemap.xml/+server.ts
+++ frontend/src/routes/sitemap.xml/+server.ts
@@ -5,29 +5,21 @@ const url = (path: string, polishPath: string | undefined = undefined): string =
     <xhtml:link rel="alternate" hreflang="en" href="https://jakub.wegrzyn.dev/${path}"/>
     <xhtml:link rel="alternate" hreflang="pl" href="https://jakub.wegrzyn.dev/pl/${polishPath || path}"/>
 </url>
-<url>
-    <loc>https://jakub.wegrzyn.dev/pl/${polishPath || path}</loc>
-    <xhtml:link rel="alternate" hreflang="en" href="https://jakub.wegrzyn.dev/${path}"/>
-    <xhtml:link rel="alternate" hreflang="pl" href="https://jakub.wegrzyn.dev/pl/${polishPath || path}"/>
-</url>
 `
 }
 export const prerender = true
 
 export async function GET() {
-    return new Response(
-        `
-    <?xml version="1.0" encoding="UTF-8" ?>
-    <urlset
-      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xhtml="https://www.w3.org/1999/xhtml"
-      xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-      xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-      xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-    >
+    return new Response(`
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xhtml="http://www.w3.org/1999/xhtml"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${url('')}
-    </urlset>`.trim(),
+</urlset>`.trim(),
         {
             headers: {
                 'Content-Type': 'application/xml'
